446ae44a78c756379c0a1d20f57c9664d47fdc29
jgabrielsanc <jesusgabriel2525@gmail.com>
2018-08-28 18:45:47 -0400

    Added assets and some logics

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { ANIMALES } from '../../data/data.animales';
+import { Animal } from '../../interfaces/animal.interface';
+
+
 
 @Component({
   selector: 'page-home',
@@ -7,7 +11,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animeles:Animal[] = [];
+
+  constructor() {
+    this.animeles = ANIMALES.splice(0);
+  }
+
+  reproducir(animal:Animal){
+    console.log(animal);
+
+    let audio = new Audio();
+    audio.src = animal.audio;
+
+    audio.load();
+    audio.play();
+
+    animal.reproduciendo = true;
+
+    setTimeout( () => {
+      animal.reproduciendo = false;
+    }, animal.duracion * 1000);
 
   }
 
